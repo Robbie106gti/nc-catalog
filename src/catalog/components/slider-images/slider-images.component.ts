62cec5869881a5313fbc20094712c6b8736ec3b5
Robbie106gti <rleeuwerink@gmail.com>
2018-06-01 16:37:21 -0700

    slider update

--- src/catalog/components/slider-images/slider-images.component.ts
+++ src/catalog/components/slider-images/slider-images.component.ts
@@ -6,49 +6,46 @@ declare var M: any;
   selector: 'slider-images',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="card large" *ngIf="param.Version; else cover">
-        <div class="carousel carousel-slider" data-indicators="true">
-            <div class="carousel-item">
-            <h2 class="padding">Spec {{ content.title}} {{ content.versions[param.Version].title }}</h2>
-                <span *ngIf="content.versions[param.Version].images; else icon">
-                    <img [src]="content.versions[param.Version].images.spec.image"
-                    [alt]="content.versions[param.Version].images.spec.image"
-                    class="responsive-img materialboxed">
-                </span>
-            </div>
-            <div class="carousel-item">
-            <h2 class="padding">{{ content.title}} {{ content.versions[param.Version].title }}</h2>
-                <span *ngIf="content.versions[param.Version].images; else icon">
-                    <img [src]="content.versions[param.Version].images.image.image"
-                    [alt]="content.versions[param.Version].images.image.image"
-                    class="responsive-img materialboxed" >
-                </span>
-            </div>
-        </div>
+<div class="card large" *ngIf="param.Version; else cover">
+  <div class="carousel carousel-slider" data-indicators="true">
+    <div class="carousel-item">
+      <h2 class="padding">Spec {{ content.title}} {{ content.versions[param.Version].title }}</h2>
+      <span *ngIf="content.versions[param.Version].images; else icon">
+        <img [src]="content.versions[param.Version].images.spec.image" [alt]="content.versions[param.Version].images.spec.image"
+          class="responsive-img materialboxed">
+      </span>
     </div>
-
-    <ng-template #cover>
-    <div class="card">
-        <div class="carousel carousel-slider" data-indicators="true">
-            <div class="carousel-item">
-                <h2 class="padding">{{ content.title }}</h2>
-                <img [src]="content.image" [alt]="content.title"
-                    class="responsive-img materialboxed" >
-            </div>
-        </div>
+    <div class="carousel-item">
+      <h2 class="padding">{{ content.title}} {{ content.versions[param.Version].title }}</h2>
+      <span *ngIf="content.versions[param.Version].images; else icon">
+        <img [src]="content.versions[param.Version].images.image.image" [alt]="content.versions[param.Version].images.image.image"
+          class="responsive-img materialboxed">
+      </span>
     </div>
-    </ng-template>
+  </div>
+</div>
 
-    <ng-template #icon>
-        <i class="material-icons large">image</i>
-    </ng-template>
+<ng-template #cover>
+  <div class="card">
+    <div class="carousel carousel-slider" data-indicators="true">
+      <div class="carousel-item">
+        <h2 class="padding">{{ content.title }}</h2>
+        <img [src]="content.image" [alt]="content.title" class="responsive-img materialboxed">
+      </div>
+    </div>
+  </div>
+</ng-template>
+
+<ng-template #icon>
+  <i class="material-icons large">image</i>
+</ng-template>
     `,
   styles: [
-    `
+    ` 
     .carousel .indicators .indicator-item {
-            background-color: #444 !important;
-            color: #444 !important;
-        }
+      background-color: #444 !important;
+      color: #444 !important;
+    }
     `
   ]
 })
