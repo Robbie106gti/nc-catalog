fceb82b31a17447797af9b5dc8bd1b6a5bf2959d
Robert Leeuwerink <rleeuwerink@gmail.com>
2018-11-14 16:24:35 -0800

    json import and firestore add trims and moldings

--- src/catalog/components/cat-content/trimsNmoldings.component.ts
+++ src/catalog/components/cat-content/trimsNmoldings.component.ts
@@ -16,45 +16,44 @@ declare var M: any;
   selector: 'trimNmoldings-content',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-<div class="col s12 m6">
-  <description-card [content]="content"></description-card>
-  <div class="card-panel grey lighten-3">
-    <span class="card-title">
-      <h4>Standards</h4>
-    </span>
-    <div class="divider"></div>
-    <ul class="collection">
-      <li class="collection-item" *ngFor="let standard of content.standards">{{standard.title}}: {{standard.content}}</li>
-    </ul>
-    <span class="card-title">
-      <h4>Options</h4>
-    </span>
-    <div class="divider"></div>
-    <ul class="collection">
-      <li class="collection-item" *ngFor="let option of content.options">{{option.title}}: {{option.content}} <a href="#!" class="secondary-content waves-effect waves-light btn">{{option.option | titlecase}}</a></li>
-    </ul>
-    <span class="card-title">
-      <h4>Restrictions</h4>
-    </span>
-    <div class="divider"></div>
-    <ul class="collection">
-      <li class="collection-item" *ngFor="let res of content.restrictions">{{res.title}}: {{res.content}}</li>
-    </ul>
-  </div>
-</div>
-
-<div class="col s12 m6">
-  <div class="card">
-    <div #slider class="carousel carousel-slider" data-indicators="true">
-      <div class="carousel-item">
-        <h2 class="padding">{{ content.title }}</h2>
-        <img #materialboxed [src]="content.image" [alt]="content.title" class="responsive-img materialboxed">
+    <div class="col s12 m6">
+      <description-card [content]="content"></description-card>
+      <div class="card-panel grey lighten-3">
+        <span class="card-title"> <h4>Standards</h4> </span>
+        <div class="divider"></div>
+        <ul class="collection">
+          <li class="collection-item" *ngFor="let standard of content.standards">
+            {{ standard.title }}: {{ standard.content }}
+          </li>
+        </ul>
+        <span class="card-title"> <h4>Options</h4> </span>
+        <div class="divider"></div>
+        <ul class="collection">
+          <li class="collection-item" *ngFor="let option of content.options">
+            {{ option.title }}: {{ option.content }}
+            <a href="#!" class="secondary-content waves-effect waves-light btn">{{ option.option | titlecase }}</a>
+          </li>
+        </ul>
+        <span class="card-title"> <h4>Restrictions</h4> </span>
+        <div class="divider"></div>
+        <ul class="collection">
+          <li class="collection-item" *ngFor="let res of content.restrictions">{{ res.title }}: {{ res.content }}</li>
+        </ul>
       </div>
     </div>
-  </div>
-</div>
-<note-item class="col s12 m6" *ngFor="let note of content.notes" [uid]="note"></note-item>
-`
+
+    <div class="col s12 m6">
+      <div class="card">
+        <div #slider class="carousel carousel-slider" data-indicators="true">
+          <div class="carousel-item">
+            <h2 class="padding">{{ content.title }}</h2>
+            <img #materialboxed [src]="content.image" [alt]="content.title" class="responsive-img materialboxed" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <note-item class="col s12 m6" *ngFor="let note of content.notes" [uid]="note"></note-item>
+  `
 })
 export class TrimMoldingContentComponent implements AfterViewInit {
   @Input()
